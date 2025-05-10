5b578d07ece90551f7a85115cda23dec70fa846c
Human <reedwith2es@gmail.com>
2025-05-09 23:15:45 -0700

    Create editor static page

---
+++ static/2025/editor/development/src/main.ts
@@ -0,0 +1,24 @@
+//
+// Experiments with Codemirror as a text editor
+//
+// @ Comments with leading @ form a rough outline
+// @ Imports
+import "./style.css";
+import { EditorState } from "@codemirror/state";
+import { EditorView, keymap } from "@codemirror/view";
+import { defaultKeymap } from "@codemirror/commands";
+import { vim } from "@replit/codemirror-vim";
+
+// @ Codemirror State Setup
+let startState = EditorState.create({
+  doc: "Hello World",
+  extensions: [vim(), keymap.of(defaultKeymap)],
+});
+
+// @ Codemirror View Setup
+let view = new EditorView({
+  state: startState,
+  parent: document.body,
+});
+
+view.focus();
